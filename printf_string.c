42414673683a1a6a6667803c79422d67d55e8d4a
MAHMOUD128 <mahmoudkhaleel551@gmail.com>
2023-04-24 08:45:50 +0200

    add _putchar.c & printf_string.c & main.c files

---
+++ printf_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * printf_string - prints a string
+ * 
+ * @val: string to print
+*/
+int printf_string(va_list val)
+{
+    char *str = va_arg(val, char *);
+    int i = 0;
+    while (str[i] != '\0'){
+        _putchar(str[i]);
+        i++;
+    }
+    return (i);
+}
